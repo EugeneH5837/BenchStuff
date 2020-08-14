5525cb31de17785c19196edde157bf42539f6fd4
Eugene Huang <eugene.huang58@gmail.com>
2020-08-14 14:54:15 -0400

    updating gitignore for ds_store and fixing index file

--- MERNStackPath/SecuringRESTApis/index.js
+++ MERNStackPath/SecuringRESTApis/index.js
@@ -36,19 +36,6 @@ app.use((req, res, next) => {
 	}
 });
 
-/* app.use((req, res, next) => {
-	if (req.headers && req.headers.authorization && req.headers.authorization.split(" ")[0] === "JWT") {
-		 jsonwebtoken.verify(req.headers.authorization.split(" ")[1], "RESTFULAPIs", (err, decode) => {
-				 if (err) req.user = undefined;
-				 req.user = decode;
-				 next();
-		 }); 
-	} else {
-			req.user = undefined;
-			next();
-	}
-}); */
-
 routes(app);
 
 // serving static files
